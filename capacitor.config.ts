7545bae2d5b8a05f061bc171e321d2e81ce54d86
Yasmine <yasmine.chaiboubi@outlook.fr>
2023-08-02 09:51:44 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TP-gestion-task',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
